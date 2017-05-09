ab0822d94f4531a3328eed50944f31a77867fa1d
bitwes <butch.wesley@gmail.com>
2017-05-08 22:58:02 -0400

    removed old code

--- scratch/cirlce_text.tres.gd
+++ scratch/cirlce_text.tres.gd
@@ -1,5 +1,3 @@
-
-
 func ready():
 	update()
 
@@ -7,14 +5,6 @@ func ready():
 func _draw():
 	var font = get_node("font_label").get_font('')
 	draw_cirlce_text('hello there friend how are you today?', font, Vector2(200, 200), 100, Color(.75, .25, .25))
-	
-func hardcoded_circle_text():
-	var font = get_node("font_label").get_font('')
-	var my_string = "Hello World"
-	var x = (360) / my_string.length()
-	for i in range(0, my_string.length()):
-	    draw_set_transform(Vector2(0, 0), deg2rad(i * x - (my_string.length()/2*x)), Vector2(1,1))
-	    draw_char(font, Vector2(0, -30), my_string[my_string.length()-i-1], "", Color(0, 0, 0))
 
 # adapted from cdqwertz's forum post at
 # https://godotdevelopers.org/forum/discussion/comment/19449#Comment_19449
